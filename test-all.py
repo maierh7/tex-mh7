a30d9751b49ef596f2bc5ccf83769e95343e4ff4
Harald Maier <harald@maierh.de>
2025-07-29 12:44:38 +0200

    Switch to test

--- test-all.py
+++ test-all.py
@@ -1,8 +1,16 @@
 #! /usr/bin/env python3
 
 import os
+import re
 from glob import glob
 import subprocess as sub
+
+
+cdir = os.getcwd ()
+m = re.search ("test$", cdir)
+# On top level change to test directory
+if m is None:
+    os.chdir ("test")
 
 for i in glob ("*.ltx"):
     cmd = "pdflatex -halt-on-error " + i + " > /dev/null"
