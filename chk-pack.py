8cc80545c07ffbd64501f29718c51804f956dec6
Harald Maier <harald@maierh.de>
2026-01-04 16:26:31 +0100

    Added

---
+++ chk-pack.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import os
+import re
+
+tp=os.environ["TEXINPUTS"]
+
+ign = (r"\.", r"tex-mh7")
+
+lst = re.split (os.pathsep, tp)
+for i in lst:
+    pr = True
+    for j in ign:
+        m = re.search (j, i)
+        #print (m)
+        if m:
+            pr = False
+            break
+    if pr is True:
+        print (i)
+        if os.path.isdir (i) is False:
+            print (i)
