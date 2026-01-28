30bdb0cd2cc8c9adcd44a87a910f47a8ffd9a159
Harald Maier <harald@maierh.de>
2026-01-28 13:58:14 +0100

    Added CT Examples
    ./test-all.py - Changed to execute 'test', 'ct_test'

--- test-all.py
+++ test-all.py
@@ -5,20 +5,24 @@ import re
 from glob import glob
 import subprocess as sub
 
+dirs = ("test", "ct_test")
 
-cdir = os.getcwd ()
-m = re.search ("test$", cdir)
-# On top level change to test directory
-if m is None:
-    os.chdir ("test")
+rdir = os.getcwd()
+print (rdir)
+def comp (dir):
+    wdir = rdir + "/" + dir
+    os.chdir (wdir)
 
-for i in glob ("*.ltx"):
-    cmd = "pdflatex -halt-on-error " + i + " > /dev/null"
-    #print (cmd)
-    try:
-        out = sub.check_output (cmd, shell = True).decode ("utf8")
-    except sub.CalledProcessError as e:
-        print ("Error on", i)
+    for i in glob ("*.[lt][et]x"):
+        cmd = "pdflatex -halt-on-error " + i + " > /dev/null"
+        # print (cmd)
+        # continue
         
-    
+        try:
+            out = sub.check_output (cmd, shell = True).decode ("utf8")
+        except sub.CalledProcessError as e:
+            print ("Error on", i)
+
+for i in dirs:
+    comp (i)
     
