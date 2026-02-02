ebee1cb329416eb9db3a2e8d1a76a40b434639f6
Harald Maier <harald@maierh.de>
2026-02-02 15:31:14 +0100

    biblatex - t9-bib.ltx

--- clean.py
+++ clean.py
@@ -3,9 +3,14 @@
 import os
 import re
 
+fns  = (
+    "t9-bib-ref.tex",
+    )
 exts = (
     ".aux",
+    ".bbl",
     ".bcf",
+    ".blg",
     ".lbl",
     ".lof",
     ".log",
@@ -20,10 +25,11 @@ for r, d, f in os.walk ("."):
     m = re.match (r"^\./\.git", r)
     if m:
         continue
+    
     for i in f:
         _, e = os.path.splitext (i)
 
-        if e in exts:
+        if e in exts or i in fns:
             fn = r + "/" + i
             print ("Delete:", fn)
             os.remove (fn)
